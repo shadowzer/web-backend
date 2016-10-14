9a0ee2e2d54bd12e9d843e9973c54c89ef038099
Denis Pro <cjbars74@gmail.com>
2016-10-14 18:56:37 +0500

    initial commit

---
+++ app/Model.php
@@ -0,0 +1,54 @@
+<?php
+
+class Model{
+
+	// здесь будем хранить название файла данных для этой модель
+	public $dataFileName;
+
+	public function __construct($modelName){
+		// конструируем название файла данных
+		// должно получиться примерно /data/modelname.json
+		$this->dataFileName=DATA_FOLDER.DS.$modelName.'.json';
+	}
+	
+	// общие методы для всех моделей
+
+	public function load($id=false){
+		// считаем файл
+		$data=file_get_contents($this->dataFileName);
+		// декодируем 
+		$data=json_decode($data);
+
+		// если id не передан - то возвращаем все записи, иначе только нужную
+		if($id===false){
+			return $data;
+		}else{
+			if(array_key_exists($id, $data)){
+				return $data[$id];	
+			}	
+		}
+		return false;
+	}
+
+
+	public function create(array $item){
+		// считываем нашу "базу данных"
+		$data=file_get_contents($this->dataFileName);
+		// декодируем
+		$data=json_decode($data);
+		// добавляем элемент
+		array_push($data, $item);
+		// сохраняем файл, и возврfщаем результат сохранения (успех или провал)
+		return file_put_contents($this->dataFileName, json_encode($data));
+	}
+
+
+	public function save($id){
+		echo 'напишите реализацию метода'; die();
+	}
+
+
+	public function delete($id){
+		echo 'напишите реализацию метода'; die();	
+	}
+}
