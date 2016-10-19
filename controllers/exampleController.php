ee5f562b86e00f1633779903cb195d2afcd11d17
shad <shadow.mr96@gmail.com>
2016-10-20 00:18:37 +0500

    added exampleController class functionality 1. added code to edit() method 2. added delete() method

--- controllers/exampleController.php
+++ controllers/exampleController.php
@@ -23,8 +23,26 @@ class exampleController extends Controller {
 	}
 
 	public function edit($id){
-		// НАПИШИТЕ РЕАЛИЗАЦИЮ метода save в классе Model
-
+		// PUT  http://localhost/?controller=example&id=2 {'title'='новый текст записи'}
+        // $id => array('id' => 2)
+		//if(isset($id['id']) && isset($_POST['title'])){
+        if(isset($id['id'])) {
+			// мы передаем в модель массив с данными
+			// модель должна вернуть boolean
+            parse_str(file_get_contents("php://input"),$post_vars);
+			$dataToEdit=array('id' => $id['id'], 'title' => $post_vars['title']);
+			$editedItem=$this->model->save($dataToEdit);
+			$this->setResponce($editedItem);
+		}
 	}	
 
+	public function delete($id) {
+        // DELETE http://localhost/?controller=example&id=2
+        // $id = array($id => 2)
+        if(isset($id['id'])) {
+            $dataToDelete = $id['id'];
+            $deletedItem = $this->model->delete($dataToDelete);
+            $this->setResponce($deletedItem);
+        }
+    }
 }
