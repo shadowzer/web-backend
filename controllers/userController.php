b69267417e9756d9730be7030e795433d1b25169
shad <shadow.mr96@gmail.com>
2016-10-20 00:53:55 +0500

    added user controller

--- controllers/userController.php
+++ controllers/userController.php
@@ -1,6 +1,6 @@
 <?php
 
-class exampleController extends Controller {
+class userController extends Controller {
 
 	public function index(){
 		$examples=$this->model->load();		// просим у модели все записи
@@ -13,10 +13,10 @@ class exampleController extends Controller {
 	}
 
 	public function add(){
-		if(isset($_POST['title'])){
+		if(isset($_POST['name']) && isset($_POST['score'])){
 			// мы передаем в модель массив с данными
 			// модель должна вернуть boolean
-			$dataToSave=array('title'=>$_POST['title']);
+			$dataToSave=array('name' => $_POST['name'], 'score' => $_POST['score']);
 			$addedItem=$this->model->create($dataToSave);
 			$this->setResponce($addedItem);
 		}
@@ -25,14 +25,15 @@ class exampleController extends Controller {
 	public function edit($id){
 		// PUT  http://localhost/?controller=example&id=2 {'title'='новый текст записи'}
         // $id => array('id' => 2)
-		//if(isset($id['id']) && isset($_POST['title'])){
         if(isset($id['id'])) {
 			// мы передаем в модель массив с данными
 			// модель должна вернуть boolean
             parse_str(file_get_contents("php://input"),$post_vars);
-			$dataToEdit=array('id' => $id['id'], 'title' => $post_vars['title']);
-			$editedItem=$this->model->save($dataToEdit);
-			$this->setResponce($editedItem);
+			if (isset($post_vars['name']) && isset($post_vars['score'])) {
+				$dataToEdit = array('id' => $id['id'], 'name' => $post_vars['name'], 'score' => $post_vars['score']);
+				$editedItem = $this->model->save($dataToEdit);
+				$this->setResponce($editedItem);
+			}
 		}
 	}	
 
