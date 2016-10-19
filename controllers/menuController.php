432a51055f20e1a53a4a4ea4975a63da4ad58111
shad <shadow.mr96@gmail.com>
2016-10-20 00:53:37 +0500

    added menu controller

--- controllers/menuController.php
+++ controllers/menuController.php
@@ -1,10 +1,10 @@
 <?php
 
-class indexController extends Controller {
+class menuController extends Controller {
 
 	public function index(){
-		$message = 'This in index page. This message is in controllers/indexController.php file';
-		$this->setResponce($message);
+		$menu = $this->model->load();
+		$this->setResponce($menu);
 	}
 		
 }
